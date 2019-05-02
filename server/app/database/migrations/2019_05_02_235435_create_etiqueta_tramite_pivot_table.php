41e3c457bda010564d82cfa50ea0a5f45aceebe6
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-02 20:55:16 -0300

    Add etiqueta_tramite pivot table migration

---
+++ server/app/database/migrations/2019_05_02_235435_create_etiqueta_tramite_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etiqueta_tramite', function (Blueprint $table) {
+            $table->integer('etiqueta_id')->unsigned()->index();
+            $table->foreign('etiqueta_id')->references('id')->on('etiquetas')->onDelete('cascade');
+            $table->integer('tramite_id')->unsigned()->index();
+            $table->foreign('tramite_id')->references('id')->on('tramites')->onDelete('cascade');
+            $table->primary(['etiqueta_id', 'tramite_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('etiqueta_tramite');
+    }
+}
