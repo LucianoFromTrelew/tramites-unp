7410141a1d9b06045b15ef5b2148b4f76a6747bb
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-02 20:42:21 -0300

    Add documento_tramite pivot table migration

---
+++ server/app/database/migrations/2019_05_02_234139_create_documento_tramite_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documento_tramite', function (Blueprint $table) {
+            $table->integer('documento_id')->unsigned()->index();
+            $table->foreign('documento_id')->references('id')->on('documentos')->onDelete('cascade');
+            $table->integer('tramite_id')->unsigned()->index();
+            $table->foreign('tramite_id')->references('id')->on('tramites')->onDelete('cascade');
+            $table->primary(['documento_id', 'tramite_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('documento_tramite');
+    }
+}
