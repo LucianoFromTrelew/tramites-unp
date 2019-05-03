6de4afd3442ed501cd38ac2254ba08f6338d4680
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-03 07:34:33 -0300

    Add relationship methods to models

--- server/app/app/Documento.php
+++ server/app/app/Documento.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Documento extends Model
 {
-    //
+    protected $fillable = ['descripcion'];
+
+    public function tramites() {
+        return $this->belongstoMany('App\Tramite');
+    }
 }
--- server/app/app/Metodo.php
+++ server/app/app/Metodo.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Metodo extends Model
 {
-    //
+    protected $fillable = ['descripcion', 'tramite_id'];
+
+    public function tramite() {
+        return $this->belongsTo('App\Tramite');
+    }
+
+    public function pasos() {
+        return $this->hasMany('App\Paso');
+    }
 }
