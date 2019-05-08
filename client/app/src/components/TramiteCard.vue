6ed6e0e98dc06052f224808e68ed9d9319775327
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-08 12:13:45 -0300

    Add TramiteCard

---
+++ client/app/src/components/TramiteCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-hover>
+    <v-card
+      @click="onClick"
+      slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="ma-2"
+    >
+      <v-card-title>
+        <div>
+          <span class="headline">{{ tramite.id }} - {{ tramite.titulo }}</span>
+          <div class="d-flex">
+            <div class="ml-2 grey--text text--darken-2">
+              <span>{{ tramite.descripcion }}</span>
+            </div>
+          </div>
+        </div>
+      </v-card-title>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: ["tramite"],
+  methods: {
+    onClick() {
+      this.$emit("tramite-clicked", this.tramite);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
