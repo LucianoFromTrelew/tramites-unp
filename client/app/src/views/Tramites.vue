315061e5a51b58ad43252c3c8c86e28c72e3ab93
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-08 12:13:52 -0300

    Add tramite list

--- client/app/src/views/Tramites.vue
+++ client/app/src/views/Tramites.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1 class="text-xs-center">Tramites</h1>
+  <v-layout row wrap>
+    <v-flex xs12 class="pa-3">
+      <h3 class="text-xs-center display-2">Tramites</h3>
+    </v-flex>
+    <v-flex xs12 lg6 xl4 v-for="tramite in tramites">
+      <TramiteCard :tramite="tramite" @tramite-clicked="onTramiteClick" />
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import TramiteCard from "@/components/TramiteCard";
+export default {
+  components: {
+    TramiteCard
+  },
+  computed: {
+    tramites() {
+      return this.$store.getters.tramites;
+    }
+  },
+  methods: {
+    onTramiteClick(tramite) {
+      this.$router.push(`/tramites/${tramite.id}`);
+    }
+  }
+};
 </script>
 
 <style scoped></style>
