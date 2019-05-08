e4a7b21d23f5e3818f38dbc848dba51bce64443b
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-08 08:17:22 -0300

    Add fallback route

--- client/app/src/router/routes.js
+++ client/app/src/router/routes.js
@@ -2,8 +2,7 @@ const routes = [
   {
     path: "/",
     name: "home",
-    //component: Home
-    component: () => import(/* webpackChunkName: "about" */ "@/views/Home.vue")
+    component: () => import("@/views/Home.vue")
   },
   {
     path: "/about",
@@ -12,6 +11,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
 ];
 
