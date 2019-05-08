690cb528ca88e573e4688183fc442932b363e9ba
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-08 10:02:52 -0300

    Add logic to store

--- client/app/src/store/index.js
+++ client/app/src/store/index.js
@@ -1,10 +1,51 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { http } from "@/utils/http";
 
 Vue.use(Vuex);
 
+const getIdAndDesc = ({ id, descripcion }) => ({ id, descripcion });
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    categorias: [],
+    etiquetas: [],
+    tramites: []
+  },
+  getters: {
+    categorias(state) {
+      return state.categorias.map(getIdAndDesc);
+    },
+    etiquetas(state) {
+      return state.categorias.map(getIdAndDesc);
+    },
+    tramites(state) {
+      return state.tramites.map(getIdAndDesc);
+    }
+  },
+  mutations: {
+    ADD_CATEGORIAS(state, categorias) {
+      state.categorias = categorias;
+    },
+    ADD_ETIQUETAS(state, etiquetas) {
+      state.etiquetas = etiquetas;
+    },
+    ADD_TRAMITES(state, tramites) {
+      state.tramites = tramites;
+    }
+  },
+  actions: {
+    async getCategorias({ commit }) {
+      const response = await http.get("categorias");
+      commit("ADD_CATEGORIAS", response.data);
+    },
+    async getEtiquetas({ commit }) {
+      const response = await http.get("etiquetas");
+      commit("ADD_ETIQUETAS", response.data);
+    },
+    async getTramites({ commit }) {
+      const response = await http.get("tramites");
+      commit("ADD_TRAMITES", response.data);
+    }
+  }
 });
