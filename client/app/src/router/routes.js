8dbeb369c7fa183441583269a15c8f0d17cc1c2c
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-08 08:50:40 -0300

    Add routes

--- client/app/src/router/routes.js
+++ client/app/src/router/routes.js
@@ -1,8 +1,22 @@
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: () => import("@/views/Home.vue")
+    //name: "home",
+    component: () => import("@/views/Home.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("@/views/Tramites.vue")
+      },
+      {
+        path: "categorias",
+        component: () => import("@/views/Categorias.vue")
+      },
+      {
+        path: "etiquetas",
+        component: () => import("@/views/Etiquetas.vue")
+      }
+    ]
   },
   {
     path: "/about",
