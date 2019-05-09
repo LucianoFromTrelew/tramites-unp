bb74262144c20a8593163abcc429f1a7c4fa1e90
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-09 12:42:38 -0300

    Add auth navigation guard

--- client/app/src/router/index.js
+++ client/app/src/router/index.js
@@ -2,10 +2,32 @@ import Vue from "vue";
 import Router from "vue-router";
 import routes from "./routes.js";
 
+import store from "@/store";
+
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes
 });
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    await store.dispatch("isLoggedIn");
+    if (!store.getters.isLoggedIn) {
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath }
+      });
+    } else {
+      next();
+    }
+  } else {
+    next(); // make sure to always call next()!
+  }
+});
+
+export default router;
