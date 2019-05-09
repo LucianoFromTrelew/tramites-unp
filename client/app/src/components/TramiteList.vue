96c7a278421891e6c39090331656c32a53fcfc22
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-09 08:57:08 -0300

    Display message if no tramites to display

--- client/app/src/components/TramiteList.vue
+++ client/app/src/components/TramiteList.vue
@@ -1,7 +1,12 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs12 lg6 xl4 v-for="tramite in tramites" :key="tramite.id">
-      <TramiteCard :tramite="tramite" @tramite-clicked="onClick" />
+    <div v-if="hasTramites">
+      <v-flex xs12 lg6 xl4 v-for="tramite in tramites" :key="tramite.id">
+        <TramiteCard :tramite="tramite" @tramite-clicked="onClick" />
+      </v-flex>
+    </div>
+    <v-flex v-else xs12>
+      <h1>No hay trámites para mostrar</h1>
     </v-flex>
   </v-layout>
 </template>
@@ -13,6 +18,11 @@ export default {
     TramiteCard
   },
   props: ["tramites"],
+  computed: {
+    hasTramites() {
+      return this.tramites.length > 0;
+    }
+  },
   methods: {
     onClick(tramite) {
       this.$emit("tramite-clicked", tramite);
