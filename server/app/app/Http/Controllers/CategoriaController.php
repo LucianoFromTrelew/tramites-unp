83eb9ad42f31500bd75c3adca010c6cf5fd0f125
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-09 08:58:02 -0300

    Fix CategoriaController auth issue

--- server/app/app/Http/Controllers/CategoriaController.php
+++ server/app/app/Http/Controllers/CategoriaController.php
@@ -9,7 +9,7 @@ class CategoriaController extends Controller
 {
     public function __construct()
     {
-        $this->middleware("auth:api")->except(['index', 'show']);
+        $this->middleware("auth:api")->except(['index', 'show', 'tramites']);
     }
     
     /**
