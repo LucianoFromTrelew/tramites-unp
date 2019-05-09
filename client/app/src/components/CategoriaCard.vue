4ff626662ebd5df3d621cf0ce438665f63264d82
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-09 09:37:41 -0300

    Add categorias list

---
+++ client/app/src/components/CategoriaCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-hover>
+    <v-card
+      @click="onClick"
+      slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      color="grey lighten-2"
+      class="ma-2"
+    >
+      <v-card-title>
+        <div>
+          <span class="headline"
+            >{{ categoria.id }} - {{ categoria.descripcion }}</span
+          >
+        </div>
+      </v-card-title>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: ["categoria"],
+  methods: {
+    onClick() {
+      this.$emit("categoria-clicked", this.categoria);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- client/app/src/views/Categorias.vue
+++ client/app/src/views/Categorias.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1 class="text-xs-center">Categorias</h1>
+  <v-layout row wrap>
+    <v-flex xs12 class="pa-3">
+      <h3 class="text-xs-center display-2">Categorias</h3>
+    </v-flex>
+    <CategoriaList
+      :categorias="categorias"
+      @categoria-clicked="onCategoriaClick"
+    />
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import CategoriaList from "@/components/CategoriaList";
+import { mapGetters } from "vuex";
+export default {
+  components: {
+    CategoriaList
+  },
+  computed: {
+    ...mapGetters(["categorias"])
+  },
+  methods: {
+    onCategoriaClick(categoria) {
+      this.$router.push(`/categorias/${categoria.id}`);
+    }
+  }
+};
 </script>
 
 <style scoped></style>
