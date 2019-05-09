197bef02a5f26e80c75de3c4bb9bcecf77077f01
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-09 08:13:55 -0300

    Add store logic for tramite actual

--- client/app/src/store/index.js
+++ client/app/src/store/index.js
@@ -15,7 +15,8 @@ export default new Vuex.Store({
   state: {
     categorias: [],
     etiquetas: [],
-    tramites: []
+    tramites: [],
+    tramiteActual: {}
   },
   getters: {
     categorias(state) {
@@ -37,6 +38,9 @@ export default new Vuex.Store({
     },
     ADD_TRAMITES(state, tramites) {
       state.tramites = tramites;
+    },
+    SET_TRAMITE_ACTUAL(state, tramite) {
+      state.tramiteActual = tramite;
     }
   },
   actions: {
@@ -51,6 +55,42 @@ export default new Vuex.Store({
     async getTramites({ commit }) {
       const response = await http.get("tramites");
       commit("ADD_TRAMITES", response.data);
+    },
+    async getTramite({ state, commit }, tramiteId) {
+      const tramite = state.tramites.find(
+        tramite => tramite.id === parseInt(tramiteId)
+      );
+
+      const categoria = (await http.get(`tramites/${tramiteId}/categoria`))
+        .data;
+
+      const etiquetas = (await http.get(`tramites/${tramiteId}/etiquetas`))
+        .data;
+
+      const documentos = (await http.get(`tramites/${tramiteId}/documentos`))
+        .data;
+
+      const requerimientos = (await http.get(
+        `tramites/${tramiteId}/requerimientos`
+      )).data;
+
+      const metodos = (await http.get(`tramites/${tramiteId}/metodos`)).data;
+
+      let pasosPorMetodo = {};
+      for (const { id } of metodos) {
+        const res = await http.get(`tramites/${tramiteId}/pasos/${id}`);
+        pasosPorMetodo[id] = res.data;
+      }
+      const tramiteActual = {
+        tramite,
+        categoria,
+        etiquetas,
+        documentos,
+        requerimientos,
+        metodos,
+        pasosPorMetodo
+      };
+      commit("SET_TRAMITE_ACTUAL", tramiteActual);
     }
   }
 });
