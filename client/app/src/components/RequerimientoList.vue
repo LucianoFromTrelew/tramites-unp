adea3295f54ce8a473fc409bfb24983ba495bb4a
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-10 11:23:10 -0300

    Add RequerimientoList

---
+++ client/app/src/components/RequerimientoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <li>
+        <template v-for="(requerimiento, i) in requerimientos">
+          <ul class="py-1">
+            {{
+              i + 1
+            }}
+            -
+            {{
+              requerimiento.descripcion
+            }}
+          </ul>
+          <v-divider></v-divider>
+        </template>
+      </li>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["requerimientos"]
+};
+</script>
+
+<style scoped></style>
