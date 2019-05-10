8a142fd870877cbafa3056617049d943e54e5d45
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-10 11:23:02 -0300

    Add EtiquetaEnTramiteList

---
+++ client/app/src/components/EtiquetaEnTramiteList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-flex>
+    <EtiquetaItem
+      v-for="(etiqueta, i) in etiquetas"
+      :etiqueta="etiqueta"
+      :key="i"
+      @delete="onDelete"
+    ></EtiquetaItem>
+  </v-flex>
+</template>
+
+<script>
+import EtiquetaItem from "@/components/EtiquetaItem";
+export default {
+  components: {
+    EtiquetaItem
+  },
+  props: ["etiquetas", "items"],
+  methods: {
+    onDelete(etiqueta) {
+      this.$emit("delete", etiqueta);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ client/app/src/components/EtiquetaItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-chip class="my-2 font-weight-medium">
+    {{ etiqueta.descripcion }}
+    <v-icon right @click="onClickClose">close</v-icon>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: ["etiqueta"],
+  methods: {
+    onClickClose() {
+      this.$emit("delete", this.etiqueta);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
