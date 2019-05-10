0afa3315fcf300a96309d431589b98d74a53dfa5
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-10 11:22:44 -0300

    Add DocumentoList

---
+++ client/app/src/components/DocumentoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <li>
+        <template v-for="(documento, i) in documentos">
+          <ul class="py-1">
+            {{
+              i + 1
+            }}
+            -
+            {{
+              documento.descripcion
+            }}
+          </ul>
+          <v-divider></v-divider>
+        </template>
+      </li>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["documentos", "items"]
+};
+</script>
+
+<style scoped></style>
