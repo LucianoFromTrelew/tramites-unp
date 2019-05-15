8f40fb2f22cd6b3199a285bba42fa8134ec59c94
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-05-15 16:31:26 -0300

    Add logout

--- client/app/src/layout/MyHeader.vue
+++ client/app/src/layout/MyHeader.vue
@@ -3,7 +3,7 @@
     <v-navigation-drawer v-model="drawer" fixed app>
       <v-list dense>
         <SidebarLink
-          v-for="(link, i) in sidebarLinks"
+          v-for="(link, i) in getSidebarLinks()"
           :link="link"
           :key="i"
           @sidebar-link-clicked="onSidebarLinkClick"
@@ -38,8 +38,25 @@ export default {
   },
   data() {
     return {
-      drawer: false,
-      sidebarLinks: [
+      drawer: false
+    };
+  },
+  methods: {
+    async onSidebarLinkClick(route) {
+      if (route === "/logout") {
+        await this.$store.dispatch("logout");
+        this.$router.push("/");
+        return;
+      }
+      this.$router.push(route);
+    },
+    getEditPath() {
+      if (this.$store.getters.isEditable) {
+        return this.$route.fullPath.concat("/edit");
+      }
+    },
+    getSidebarLinks() {
+      const sidebarLinks = [
         {
           route: "/",
           icon: "home",
@@ -60,17 +77,15 @@ export default {
           icon: "person",
           label: "Administración"
         }
-      ]
-    };
-  },
-  methods: {
-    onSidebarLinkClick(route) {
-      this.$router.push(route);
-    },
-    getEditPath() {
-      if (this.$store.getters.isEditable) {
-        return this.$route.fullPath.concat("/edit");
+      ];
+      if (this.$store.getters.isLoggedIn) {
+        sidebarLinks.push({
+          route: "/logout",
+          icon: "exit_to_app",
+          label: "Salir"
+        });
       }
+      return sidebarLinks;
     }
   }
 };
